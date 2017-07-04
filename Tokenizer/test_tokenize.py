bfd773946d0d4696991d547ca6dcec1e8f44dfd5
Hans <hanscje@ulrik.uio.no>
2017-07-04 14:01:11 +0200

    Added several test for tokenize.

--- Tokenizer/test_tokenize.py
+++ Tokenizer/test_tokenize.py
@@ -6,8 +6,24 @@ from PySearch.Tokenizer.tokenize import tokenize
 
 class TestTokenize(TestCase):
 
-    def test_tokenize(self):
+    def test_tokenize_simple(self):
         self.assertEqual(tokenize("dette er en test"), ["dette", "er", "en", "test"])
+        self.assertEqual(tokenize("hei"), ["hei"])
+        self.assertEqual(tokenize(""), [])
+        self.assertEqual(tokenize(" "), [])
+        self.assertEqual(tokenize("     \n\t"), [])
+
+    def test_tokenize_invalid_args(self):
+        with self.assertRaises(AssertionError):
+            tokenize(45)
+
+    def test_tokenize_basic_seperation(self):
+        self.assertEqual(tokenize("test(noe)"), ["test", "(", "noe", ")"])
+        self.assertEqual(tokenize("test(noe!)"), ["test", "(", "noe!", ")"])
+        self.assertEqual(tokenize("test.noe,hei."), ["test", ".", "noe", ",", "hei", "."])
+
+    def test_tokenize_advanced_seperation(self):
+        self.assertEqual(tokenize('"""test(noe)"""'), ['"""', 'test', '(', 'noe', ')', '"""'])
 
 
 
--- Tokenizer/tokenize.py
+++ Tokenizer/tokenize.py
@@ -1,12 +1,17 @@
 """
-Tokenizes words.
+Tokenize words.
 
 @author hjenvild@gmail.com, arildlil@protonmail.com
 """
 
 
 def tokenize(line):
-    #split_chars = ["\\", "'", ")"]
+
+    # split_chars = ["\\", "'", ")"]
+
+
+    assert isinstance(line, str), "The input is not a string, was a %r"%type(line)
+
     ord = line.strip().split()
     return ord
 
